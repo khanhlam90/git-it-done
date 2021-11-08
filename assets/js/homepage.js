cb257058feef15d72462d5fe576ad5a2a6817fa1
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-11-07 20:59:21 -0800

    Search repos by username and display repos with issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,9 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -26,33 +29,40 @@ var getUserRepos = function(user) {
     // });
 
   // upadate from (2) above - this is to to handle network errors - .catch()
+  // make a get request to url
   fetch(apiUrl)
     .then(function(response) {
       // request was successful
       if (response.ok) {
+        console.log(response);
         response.json().then(function(data) {
+          console.log(data);
           displayRepos(data, user);
         });
       } else {
-        alert('Error: GitHub User Not Found');
+        alert('Error: ' + response.statusText);
       }
     })
     .catch(function(error) {
       // Notice this `.catch()` getting chained onto the end of the `.then()` method
-      alert("Unable to connect to GitHub");
+      alert('Unable to connect to GitHub');
   });
 };
 
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
+
 
 var formSubmitHandler = function(event) {
+  // prevent page from refreshing
   event.preventDefault();
+
   // get value from input element
 var username = nameInputEl.value.trim();
 
 if (username) {
   getUserRepos(username);
+
+  // clear old content
+  repoContainerEl.textContent = '';
   nameInputEl.value = "";
 } else {
   alert("Please enter a GitHub username");
@@ -68,11 +78,7 @@ var displayRepos = function(repos, searchTerm) {
     return;
   }
 
-  // clear old content
-  repoContainerEl.textContent = "";
-  repoSearchTerm.textContent = searchTerm;
-  console.log(repos);
-  console.log(searchTerm);
+    repoSearchTerm.textContent = searchTerm;
 
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
@@ -111,5 +117,5 @@ repoEl.appendChild(statusEl);
 }
 };
 
-
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
